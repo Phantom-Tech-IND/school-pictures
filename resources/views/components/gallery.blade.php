e80ed64db5a7115b1f4c770dcd22648563aafd5c
infinityatom <19624654+bodnarescu-riccardo@users.noreply.github.com>
2024-04-18 14:59:46 +0300

    overlapping text

--- resources/views/components/gallery.blade.php
+++ resources/views/components/gallery.blade.php
@@ -1,16 +1,16 @@
 <section class="text-gray-600 body-font">
     <div class="container flex flex-wrap px-5 py-24 mx-auto">
         <div class="flex flex-wrap w-full mb-20">
-            <h1 class="mb-4 text-2xl font-medium text-gray-900 sm:text-3xl title-font lg:w-1/3 lg:mb-0">
+            <h1 class="mb-4 text-2xl font-medium text-gray-900 sm:text-3xl title-font 2xl:w-1/3 lg:mb-0">
                 {{ $title }}</h1>
-            <p class="mx-auto text-base leading-relaxed lg:pl-6 lg:w-2/3">{{ $description }}</p>
+            <p class="mx-auto text-base leading-relaxed 2xl:pl-6 2xl:w-2/3">{{ $description }}</p>
         </div>
 
         <div class="grid grid-cols-2 lg:grid-cols-4 gap-4 bob">
             @foreach ($images as $image)
                 <a class="h-full w-full rounded-lg object-cover bg-black group" data-fslightbox="{{ $gallery_name }}"
                     href="{{ asset($image) }}">
-                    <img class="h-full w-full rounded-lg object-cover bg-black transition-opacity duration-300 group-hover:opacity-50"
+                    <img class="h-full w-full rounded-lg object-cover bg-black transition-opacity duration-300 group-hover:opacity-70"
                         src="{{ asset($image) }}"
                         alt="ArtLine Fotografie AG">
                 </a>
