6442946444c54a40d357206d5460b632a6fa0972
infinityatom <19624654+bodnarescu-riccardo@users.noreply.github.com>
2024-04-18 17:48:57 +0300

    added constants

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+
+        \App\Models\User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@schoolpictures.com',
+            'password' => bcrypt('parola123'),
+        ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Martina',
+            'email' => 'martina@schoolpictures.com',
+            'password' => bcrypt('Password123!!'),
+        ]);
+
+        \App\Models\Message::factory(10)->create();
+        \App\Models\Institution::factory(2)->create();
+        \App\Models\Offers::factory(10)->create();
+
+        $this->call([
+            ProductSeeder::class,
+        ]);
+
+    }
+}
