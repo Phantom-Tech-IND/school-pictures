82e9d6baf8ffb771439367cb76986819dc87804d
Axentioi Alexandru <axentioialexandru95@gmail.com>
2024-04-19 15:05:11 +0300

    Added latest

--- app/Http/Controllers/Filament/GuestPanelController.php
+++ app/Http/Controllers/Filament/GuestPanelController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Filament;
 
+use App\Constants\Constants;
 use App\Http\Controllers\Controller;
 use App\Models\Message;
 use App\Models\Offers;
@@ -91,7 +92,37 @@ class GuestPanelController extends Controller
 
     public function viewForTestingPurposes(Request $request)
     {
-        return view('view-for-testing-purposes');
+
+        $images = [
+            Constants::KINDERGARDEN_IMAGE_URL.'SH-7321-6607.jpg',
+            Constants::KINDERGARDEN_IMAGE_URL.'SH-7219-8137-9257.jpg',
+            Constants::KINDERGARDEN_IMAGE_URL.'KG-7157-8475-4031.jpg',
+            Constants::KINDERGARDEN_IMAGE_URL.'SH-7321-7038.jpg',
+            Constants::KINDERGARDEN_IMAGE_URL.'SH-7333-8904-7425.jpg',
+            Constants::KINDERGARDEN_IMAGE_URL.'SH-7326-8999-3821.jpg',
+            // Constants::KINDERGARDEN_IMAGE_URL.'KG-2115-6516-2.jpg',
+            // Constants::KINDERGARDEN_IMAGE_URL.'SH-7333-8904-7433.jpg',
+            // Constants::KINDERGARDEN_IMAGE_URL.'SH-7336-9040-8250.jpg',
+            // Constants::KINDERGARDEN_IMAGE_URL.'SH-7325-8963-3080.jpg',
+            // Constants::KINDERGARDEN_IMAGE_URL.'KG-7157-8475-4033.jpg',
+            // Constants::KINDERGARDEN_IMAGE_URL.'SH-7326-8999-3828.jpg',
+            // Constants::KINDERGARDEN_IMAGE_URL.'IMG_9936-1.jpg',
+        ];
+
+        $productImages = [
+            Constants::KINDERGARDEN_IMAGE_URL.'SH-7321-8492-6640.jpg',
+            Constants::KINDERGARDEN_IMAGE_URL.'D5A7060.jpg',
+            Constants::KINDERGARDEN_IMAGE_URL.'SH-7321-8884-6851.jpg',
+            Constants::KINDERGARDEN_IMAGE_URL.'KG-1700-2376-1.jpg',
+            Constants::KINDERGARDEN_IMAGE_URL.'KG-7150-8063-0840-2.jpg',
+            Constants::KINDERGARDEN_IMAGE_URL.'MG_1665.jpg',
+            Constants::KINDERGARDEN_IMAGE_URL.'KG-2408-2572_-1.jpg',
+            Constants::KINDERGARDEN_IMAGE_URL.'SH-7325-8963-3073.jpg',
+            Constants::KINDERGARDEN_IMAGE_URL.'KG-7385-9689-8014.jpg',
+            Constants::KINDERGARDEN_IMAGE_URL.'SH-7219-8137-9260.jpg',
+        ];
+
+        return view('view-for-testing-purposes', compact('images'), compact('productImages'));
     }
 
     public function galleryCode(Request $request)
