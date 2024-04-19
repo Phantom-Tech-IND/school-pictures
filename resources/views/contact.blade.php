b4f8b8d87960c34632cce31315036748de1bfd2e
infinityatom <19624654+bodnarescu-riccardo@users.noreply.github.com>
2024-04-19 10:04:07 +0300

    created ContactForm and LocationInfo

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,4 +1,13 @@
 @extends('layouts.app')
 @section('content')
-    <x-hero-banner />
+    {{-- <iframe
+        class="w-full h-screen box-border"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10861.469461779034!2d8.072933718514744!3d47.111471535971205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478fde276a6f0f23%3A0x5f29a9e362137116!2sGewerbezone%2059%2C%206018%20Buttisholz%2C%20Switzerland!5e0!3m2!1sen!2sro!4v1713497353069!5m2!1sen!2sro"
+        style="border:0;" allowfullscreen loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade">
+    </iframe> --}}
+    <div class="mt-20">
+        @include('components.contact-form')
+        {{-- @include('components.location-info') --}}
+    </div>
 @endsection
