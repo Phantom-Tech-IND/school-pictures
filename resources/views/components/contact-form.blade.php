f0b6d24b0a232ced13b36dfb656b54776493324b
infinityatom <19624654+bodnarescu-riccardo@users.noreply.github.com>
2024-04-19 10:04:07 +0300

    created ContactForm and LocationInfo

---
+++ resources/views/components/contact-form.blade.php
@@ -0,0 +1,29 @@
+<div>
+    <h1>KONTAKTFORMULAR</h1>
+    <h4>Optional können Sie aus den Bereichen auswählen:</h4>
+    <div>
+        <span>Bewerbungen</span>
+        <span>Portrait, Familie u. Friends</span>
+        <span>Halb,-Akt u. Babybauch</span>
+        <span>Gebäudeaufnahmen und Architektu</span>
+
+        <span>Kalendershooting</span>
+        <span>Lichtshooting</span>
+        <span>Kinder & Schule</span>
+        <span>Portraitbilder für Firmen</span>
+
+        <span>Rainshooting</span>
+        <span>Lehrlingsshooting</span>
+        <span>Hochzeitsfotografie</span>
+        <span>Gruppenbilder für Firmen</span>
+    </div>
+
+    <p>Bewerbungsbild - Fotoshooting- Terminwunsch: z.B.: 2.2.2022, 10:00 bis 11:30 Uhr</p>
+    <p>Ihr Name (Pflichtfeld)</p>
+    <p>Ihre E-Mail-Adresse (Pflichtfeld)</p>
+    <p>Betreff: Zu welchem Bereich haben Sie Fragen?</p>
+
+    <h4>Ihre Nachricht</h4>
+
+    <p>Wir bieten Ihnen Bewerbungsbild - Fotoshooting's, Studio- und Outdoor Fotoshootings von Familien, Babys &amp; Kindern, Frauen, Männern, Partnern, Best Friends an. Professionelle Kindergarten- und Schulfotografie, Gebäudeaufnahmen-/ Architektur,  Reportagen und Hochzeitfotografie , Gruppenbilder für Firmen, Portraitbilder für Firmen, als auch Sach- und Produktfotografie. Unsere Geschenk  - Highlights sind spezielle Shootingsets für die Lichtmalerei, Kalender- und Rainshootings, der Akt &amp;  Halbakt-,  Babybauch- und Tierfotografie.</p>
+</div>
---
+++ resources/views/components/location-info.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <h2>KREATIVE FOTOSHOOTINGS IM STUDIO UND DRAUSSEN, IN BUTTISHOLZ.</h2>
+    <h3>ArtLine Fotografie AG</h3>
+    <p>Gewerbezone 59 in 6018 Buttisholz</p>
+    <p>+41 41 921 40 25</p>
+    <p>Öffnungszeiten:</p>
+    <p>Termine nach Vereinbarung</p>
+</div>
