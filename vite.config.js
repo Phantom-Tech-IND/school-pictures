facce1f58210d78ed8d07fdc332f115e3282d7c1
Axentioi Alexandru <axentioialexandru95@gmail.com>
2024-04-20 17:51:03 +0300

    Changed into js component

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         laravel({
             input: [
                 "resources/css/app.css",
-                "resources/js/image-selection.js",
+                "resources/js/image-selector/index.js",
                 "resources/js/app.js",
                 "resources/css/filament/admin/theme.css",
             ],
