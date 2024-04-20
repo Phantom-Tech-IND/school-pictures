ba2a69c5de34276471e9208eb46e90db6a17dd14
infinityatom <19624654+bodnarescu-riccardo@users.noreply.github.com>
2024-04-20 16:41:04 +0300

    added adaptive design for image-gallery-container and an example of how to get the data from it

--- resources/views/components/image-selector.blade.php
+++ resources/views/components/image-selector.blade.php
@@ -4,13 +4,16 @@
 
 <template id="image-selector-template">
     <div class="border cursor-pointer hover:border-blue-500 focus:border-blue-700 focus:outline-none">
+        <!-- Adjusted class to ensure images are square and of the same size -->
         <img id="image"
-            class="object-cover w-full h-48 transition duration-300 ease-in-out transform rounded-lg hover:scale-105">
+            class="object-cover w-full h-full transition duration-300 ease-in-out transform">
     </div>
 </template>
 
 <template id="image-display-template">
-    <div id="selectedImagesList" class="flex flex-row flex-wrap justify-start gap-4 mt-8"></div>
+    <div id="selectedImagesList"
+        class="flex justify-start gap-4 mt-8 overflow-x-auto whitespace-nowrap scroll-smooth scrollbar-hide">
+    </div>
 </template>
 
 
@@ -21,4 +24,3 @@
         customElements.define('image-display', ImageDisplay);
     </script>
 @endonce
-
