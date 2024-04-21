c05c0ddf9eb4585edfe9b6fc9329addf89fcaca8
infinityatom <19624654+bodnarescu-riccardo@users.noreply.github.com>
2024-04-21 09:33:37 +0300

    forgot to uncomment the map

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 @section('content')
-    {{-- <iframe
+    <iframe
         class="box-border w-full h-screen"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10861.469461779034!2d8.072933718514744!3d47.111471535971205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478fde276a6f0f23%3A0x5f29a9e362137116!2sGewerbezone%2059%2C%206018%20Buttisholz%2C%20Switzerland!5e0!3m2!1sen!2sro!4v1713497353069!5m2!1sen!2sro"
         style="border:0;" allowfullscreen loading="lazy"
         referrerpolicy="no-referrer-when-downgrade">
-    </iframe> --}}
-    <div class="flex flex-col justify-center mt-32 lg:flex-row">
+    </iframe>
+    <div class="flex flex-col justify-center mt-12 lg:flex-row">
         @include('components.contact-form', ['class' => 'w-full md:w-2/3'])
         @include('components.location-info', ['class' => 'w-full md:w-1/3'])
     </div>
