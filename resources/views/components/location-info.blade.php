db48ce74ba2ce55b77cdf1970f1153cb32c7fbb6
infinityatom <19624654+bodnarescu-riccardo@users.noreply.github.com>
2024-04-21 09:29:55 +0300

    finished styling

--- resources/views/components/location-info.blade.php
+++ resources/views/components/location-info.blade.php
@@ -1,8 +1,33 @@
-<div>
-    <h2>KREATIVE FOTOSHOOTINGS IM STUDIO UND DRAUSSEN, IN BUTTISHOLZ.</h2>
-    <h3>ArtLine Fotografie AG</h3>
-    <p>Gewerbezone 59 in 6018 Buttisholz</p>
-    <p>+41 41 921 40 25</p>
-    <p>Öffnungszeiten:</p>
-    <p>Termine nach Vereinbarung</p>
+<div class="max-w-xl p-4 mx-auto bg-gray-100 rounded-lg shadow lg:max-w-sm lg:m-0">
+    <h2 class="mb-2 text-xl font-bold">Kreative Fotoshootings im Studio und draussen, in Buttisholz</h2>
+    <div class="sticky flex flex-col mt-10 top-32">
+        <h3 class="mb-4 text-lg font-bold">ArtLine Fotografie AG</h3>
+        <label class="mb-1 font-semibold">
+            Adresse:
+            <a href="https://www.google.com/maps/search/?api=1&query=Gewerbezone+59,+6018+Buttisholz" target="_blank" class="mb-4 text-green-500 hover:text-green-700 focus:text-green-700">
+                Gewerbezone 59, 6018 Buttisholz
+            </a>
+        </label>
+        <label class="mb-1 font-semibold">
+            Telefon:
+            <a href="tel:+41419214025" class="mb-4 text-green-500 hover:text-green-700 focus:text-green-700">
+                +41 41 921 40 25
+            </a>
+        </label>
+        <label class="mb-1 font-semibold">
+            E-Mail:
+            <a href="mailto:artline@email.com" class="text-green-500 hover:text-green-700 focus:text-green-700">
+                artline@email.com
+            </a>
+        </label>
+        <label class="mb-1 font-semibold">
+            Öffnungszeiten:<br>
+            <div class="flex w-full lg:justify-between">
+                <p class="whitespace-nowrap">Mo-Fr: 09:00 - 18:00</p>
+                <p class="mx-2">|</p>
+                <p class="whitespace-nowrap">Sa: 10:00 - 14:00</p>
+            </div>
+        </label>
+        <p class="mb-4 text-center">Termine nach Vereinbarung</p>
+    </div>
 </div>
