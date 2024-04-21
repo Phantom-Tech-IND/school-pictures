886c54c422e66816879e9a1cd3a50b10ecc29c54
Axentioi Alexandru <axentioialexandru95@gmail.com>
2024-04-21 20:53:18 +0300

    Solved min-width

--- resources/views/components/image-selector.blade.php
+++ resources/views/components/image-selector.blade.php
@@ -11,7 +11,7 @@
 
 <template id="image-display-template">
     <div id="selectedImagesList"
-        class="flex justify-start gap-4 mt-8">
+        class="flex justify-start gap-4 py-4 mt-8 overflow-x-auto">
         <!-- Styles for images within this container are defined in CSS -->
     </div>
 </template>
@@ -24,6 +24,6 @@
                 <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
             </svg>
         </button>
-        <img class="object-cover w-full h-full">
+        <img class="object-cover w-full h-full min-w-32">
     </div>
 </template>
