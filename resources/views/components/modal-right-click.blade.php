1702bbd42217f5fa7213dfe2b990c342e7199d57
infinityatom <19624654+bodnarescu-riccardo@users.noreply.github.com>
2024-04-22 21:06:20 +0300

    Added icon

--- resources/views/components/modal-right-click.blade.php
+++ resources/views/components/modal-right-click.blade.php
@@ -1,5 +1,13 @@
 <dialog id="disable-right-click-modal">
-    <div>This is a modal</div>
+    <div
+        class="flex items-center justify-center flex-shrink-0 w-12 h-12 mx-auto bg-red-100 rounded-full sm:mx-0 sm:h-10 sm:w-10">
+        <svg class="w-6 h-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+        </svg>
+    </div>
+    <div>This function has been disabled for <b>ArtLine Fotografie AG -Fotografin- Tanja Arnold.</b></div>
     <button onclick="document.getElementById('disable-right-click-modal').close()">Close</button>
 </dialog>
 
