737a3c18878e48c1557f5195444c808002a4f597
infinityatom <19624654+bodnarescu-riccardo@users.noreply.github.com>
2024-04-22 14:17:16 +0300

    Added pages and routed them

---
+++ resources/views/frequently-asked-questions.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+    <div class="mt-20">
+        FAQ here
+    </div>
+@endsection
---
+++ resources/views/impressum.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+    <div class="mt-20">
+        impressum here
+    </div>
+@endsection
