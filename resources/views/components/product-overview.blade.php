256aac638488d3a8ba5892206e906d7f0461fcd8
infinityatom <19624654+bodnarescu-riccardo@users.noreply.github.com>
2024-04-23 07:34:51 +0300

    Refactor product overview to enhance interactivity and visual feedbac

--- resources/views/components/product-overview.blade.php
+++ resources/views/components/product-overview.blade.php
@@ -20,43 +20,30 @@
                 <h2 class="sr-only">Product information</h2>
                 <p class="text-3xl tracking-tight text-gray-900">$192</p>
 
-                <div class="mt-6">
+                <div class="mt-6" x-data="{ rating: 4, maxRating: 5, tempRating: null }">
                     <h3 class="sr-only">Reviews</h3>
                     <div class="flex items-center">
-                        <div class="flex items-center">
-
-                            <svg class="flex-shrink-0 w-5 h-5 text-gray-900" viewBox="0 0 20 20" fill="currentColor"
-                                aria-hidden="true">
-                                <path fill-rule="evenodd"
-                                    d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
-                                    clip-rule="evenodd" />
-                            </svg>
-                            <svg class="flex-shrink-0 w-5 h-5 text-gray-900" viewBox="0 0 20 20" fill="currentColor"
-                                aria-hidden="true">
-                                <path fill-rule="evenodd"
-                                    d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
-                                    clip-rule="evenodd" />
-                            </svg>
-                            <svg class="flex-shrink-0 w-5 h-5 text-gray-900" viewBox="0 0 20 20" fill="currentColor"
-                                aria-hidden="true">
-                                <path fill-rule="evenodd"
-                                    d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
-                                    clip-rule="evenodd" />
-                            </svg>
-                            <svg class="flex-shrink-0 w-5 h-5 text-gray-900" viewBox="0 0 20 20" fill="currentColor"
-                                aria-hidden="true">
-                                <path fill-rule="evenodd"
-                                    d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
-                                    clip-rule="evenodd" />
-                            </svg>
-                            <svg class="flex-shrink-0 w-5 h-5 text-gray-200" viewBox="0 0 20 20" fill="currentColor"
-                                aria-hidden="true">
-                                <path fill-rule="evenodd"
-                                    d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
-                                    clip-rule="evenodd" />
-                            </svg>
+                        <div class="flex items-center gap-2">
+                            <template x-for="(star, index) in Array.from({ length: maxRating }, (_, i) => i + 1)"
+                                :key="index">
+                                <svg @click="rating = index + 1; tempRating = null" @mouseover="tempRating = index + 1"
+                                    @mouseleave="tempRating = null"
+                                    :class="{ 'w-6 h-6': tempRating === index + 1, 'w-5 h-5': tempRating !== index + 1 }"
+                                    class="flex-shrink-0 text-yellow-400 transition-transform duration-150 ease-in-out cursor-pointer"
+                                    style="margin: -0.125rem" <!-- Adjust margin to compensate for size change -->
+                                    viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                    <path
+                                        :fill="tempRating !== null ? (tempRating >= index + 1 ? 'currentColor' : 'none') : (
+                                            rating >= index + 1 ? 'currentColor' : 'none')"
+                                        stroke="currentColor" stroke-width="1" fill-rule="evenodd"
+                                        d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            </template>
                         </div>
-                        <p class="sr-only">4 out of 5 stars</p>
+                        <p class="sr-only"
+                            x-text="(tempRating !== null ? tempRating : rating) + ' out of ' + maxRating + ' stars'">
+                        </p>
                         <a href="#" class="ml-3 text-sm font-medium text-accent-600 hover:text-accent-500">117
                             reviews</a>
                     </div>
@@ -78,10 +65,14 @@
                                     ];
                                 @endphp
                                 @foreach ($colors as $index => $color)
-                                    <label class="relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none ring-gray-400">
-                                        <input type="radio" name="color-choice" value="{{ $color['value'] }}" class="sr-only" aria-labelledby="color-choice-{{ $index }}-label">
-                                        <span id="color-choice-{{ $index }}-label" class="sr-only">{{ $color['value'] }}</span>
-                                        <span aria-hidden="true" class="{{ $color['bg'] }} border border-black rounded-full {{ $color['border'] }} w-8 h-8"></span>
+                                    <label
+                                        class="relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none ring-gray-400">
+                                        <input type="radio" name="color-choice" value="{{ $color['value'] }}"
+                                            class="sr-only" aria-labelledby="color-choice-{{ $index }}-label">
+                                        <span id="color-choice-{{ $index }}-label"
+                                            class="sr-only">{{ $color['value'] }}</span>
+                                        <span aria-hidden="true"
+                                            class="{{ $color['bg'] }} border border-black rounded-full {{ $color['border'] }} w-8 h-8"></span>
                                     </label>
                                 @endforeach
                             </div>
@@ -91,18 +82,29 @@
                     <div x-data="{ selectedProductId: '{{ $selectedProductId }}' }" class="mt-10">
                         <div class="flex items-center justify-between">
                             <h3 class="text-sm font-medium text-gray-900">Products</h3>
-                            <a href="#" class="text-sm font-medium text-accent-600 hover:text-accent-500">Info about products</a>
+                            <a href="#" class="text-sm font-medium text-accent-600 hover:text-accent-500">Info
+                                about products</a>
                         </div>
 
                         <fieldset class="mt-4">
                             <legend class="sr-only">Choose a product</legend>
                             <div class="grid grid-cols-4 gap-4 sm:grid-cols-6 lg:grid-cols-4">
                                 @foreach ($products as $product)
-                                    <label :class="{'ring-2 ring-accent-500': selectedProductId == '{{ $product['id'] }}', 'text-gray-900 bg-white': selectedProductId != '{{ $product['id'] }}'}" class="relative flex items-center justify-center h-32 overflow-hidden text-sm font-medium uppercase border rounded-md shadow-sm cursor-pointer group hover:bg-gray-50 focus:outline-none sm:flex-1">
-                                        <input type="radio" name="product-choice" value="{{ $product['id'] }}" class="sr-only" x-model="selectedProductId" aria-labelledby="product-choice-{{ $product['id'] }}-label">
-                                        <span id="product-choice-{{ $product['id'] }}-label" class="sr-only">{{ $product['id'] }}</span>
-                                        <img src="{{ $product['image'] }}" alt="" class="object-cover w-full h-full transition-opacity duration-300 ease-in-out rounded-md">
-                                        <div class="absolute inset-0 transition-all duration-300 ease-in-out bg-black bg-opacity-0 group-hover:bg-opacity-50"></div>
+                                    <label
+                                        :class="{ 'ring-2 ring-accent-500': selectedProductId ==
+                                            '{{ $product['id'] }}', 'text-gray-900 bg-white': selectedProductId !=
+                                                '{{ $product['id'] }}' }"
+                                        class="relative flex items-center justify-center h-32 overflow-hidden text-sm font-medium uppercase border rounded-md shadow-sm cursor-pointer group hover:bg-gray-50 focus:outline-none sm:flex-1">
+                                        <input type="radio" name="product-choice" value="{{ $product['id'] }}"
+                                            class="sr-only" x-model="selectedProductId"
+                                            aria-labelledby="product-choice-{{ $product['id'] }}-label">
+                                        <span id="product-choice-{{ $product['id'] }}-label"
+                                            class="sr-only">{{ $product['id'] }}</span>
+                                        <img src="{{ $product['image'] }}" alt=""
+                                            class="object-cover w-full h-full transition-opacity duration-300 ease-in-out rounded-md">
+                                        <div
+                                            class="absolute inset-0 transition-all duration-300 ease-in-out bg-black bg-opacity-0 group-hover:bg-opacity-50">
+                                        </div>
                                     </label>
                                 @endforeach
                             </div>
