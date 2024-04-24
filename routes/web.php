16171e65d9ac47c404c61e1922cb1bc4ab47f258
Axentioi Alexandru <axentioialexandru95@gmail.com>
2024-04-24 14:43:33 +0300

    Refactored routes

--- routes/web.php
+++ routes/web.php
@@ -14,23 +14,26 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::controller(GuestPanelController::class)->group(function () {
+    Route::get('/', 'index')->name('home');
+    Route::get('/shop', 'shop')->name('shop');
+    Route::get('/offer', 'offer')->name('offer');
+    Route::get('/product', 'product')->name('product');
+    Route::get('/not-available', 'notAvailable')->name('not-available');
+    Route::get('/gallery-code', 'galleryCode')->name('gallery-code');
+    Route::get('/contact', 'contact')->name('contact');
+    Route::get('/offers', 'offers')->name('offers');
+    Route::get('/kindergarten-und-schulfotografie', 'kindergarden')->name('kindergarden');
+    Route::get('/upload', 'upload');
+    Route::get('/team', 'team')->name('team');
+    Route::get('/partners', 'partners')->name('partners');
+    Route::get('/view-for-testing-purposes', 'viewForTestingPurposes')->name('view-for-testing-purposes');
+    Route::get('/haeufig-gestellte-fragen-faq', 'frequentlyAskedQuestions')->name('frequently-asked-questions');
+    Route::get('/impressum', 'impressum')->name('impressum');
+    Route::get('/agb', 'generalTermsAndConditions')->name('general-terms-and-conditions');
+    Route::post('/contact/submit', 'postContactForm')->name('contact.submit');
+});
 
-Route::get('/', [GuestPanelController::class, 'index'])->name('home');
 Route::get('/cart', [CartController::class, 'index'])->name('cart');
-Route::get('/shop', [GuestPanelController::class, 'shop'])->name('shop');
-Route::get('/offer', [GuestPanelController::class, 'offer'])->name('offer');
-Route::get('/product', [GuestPanelController::class, 'product'])->name('product');
-Route::get('/not-available', [GuestPanelController::class, 'notAvailable'])->name('not-available');
-Route::get('/gallery-code', [GuestPanelController::class, 'galleryCode'])->name('gallery-code');
-Route::get('/contact', [GuestPanelController::class, 'contact'])->name('contact');
-Route::get('/offers', [GuestPanelController::class, 'offers'])->name('offers');
-Route::get('/kindergarten-und-schulfotografie', [GuestPanelController::class, 'kindergarden'])->name('kindergarden');
-Route::get('/upload', [GuestPanelController::class, 'upload']);
-Route::get('/team', [GuestPanelController::class, 'team'])->name('team');
-Route::get('/partners', [GuestPanelController::class, 'partners'])->name('partners');
-Route::get('/view-for-testing-purposes', [GuestPanelController::class, 'viewForTestingPurposes'])->name('view-for-testing-purposes');
-Route::get('/haeufig-gestellte-fragen-faq', [GuestPanelController::class, 'frequentlyAskedQuestions'])->name('frequently-asked-questions');
-Route::get('/impressum', [GuestPanelController::class, 'impressum'])->name('impressum');
-Route::get('/agb', [GuestPanelController::class, 'generalTermsAndConditions'])->name('general-terms-and-conditions');
+
 Route::post('/students/sync-photos', 'StudentController@syncPhotos')->name('students.sync-photos');
-Route::post('/contact/submit', [GuestPanelController::class, 'postContactForm'])->name('contact.submit');
