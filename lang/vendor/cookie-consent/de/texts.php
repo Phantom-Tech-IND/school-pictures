e1db46057e1c922538f1e504d44b93d20f64ddef
infinityatom <19624654+bodnarescu-riccardo@users.noreply.github.com>
2024-04-25 09:43:55 +0300

    Add Spatie Cookie Consent package and middleware for EU compliance

---
+++ lang/vendor/cookie-consent/de/texts.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => 'Cookies machen unser Leben angenehmer',
+    'message' => 'Wir verwenden Cookies und andere Technologien, um unsere Websites sicher und effizient zu betreiben, Leistung zu überwachen und relevante Inhalte bereitzustellen. Mit Ihrer Zustimmung nutzen wir diese Daten für Analyse, Marketing und Einbindung externer Inhalte, wobei Daten auch an Dritte weitergegeben werden können. Es besteht das Risiko, dass bei Datenübermittlung in Drittländer Behörden diese Daten zugreifen und verarbeiten können. Durch Klicken auf „Geht klar“ stimmen Sie der Nutzung und Datenübermittlung zu. Ihre Zustimmung ist jederzeit widerrufbar. Details in unserer Cookie- und Datenschutzrichtlinie.',
+    'agree' => 'Geht klar',
+
+    'cookie_policy' => 'Cookie-Richtlinie',
+    'privacy_policy' => 'Datenschutz',
+    'imprint' => 'Impressum',
+];
