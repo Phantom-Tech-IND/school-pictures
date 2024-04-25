765614dae9bed774a2ad725c07f0ea9f2eba97ee
infinityatom <19624654+bodnarescu-riccardo@users.noreply.github.com>
2024-04-25 10:27:39 +0300

    Changed privacy/cookie policy layout

--- resources/views/privacy-policy.blade.php
+++ resources/views/privacy-policy.blade.php
@@ -1,8 +1,16 @@
 @extends('layouts.app')
-
 @section('content')
-    <div class="container px-4 mx-auto">
-        <h1 class="my-4 text-xl font-bold">Privacy Policy</h1>
-        <p>This is the Privacy Policy page. Here you should detail how you handle user data and privacy.</p>
+    <div class="px-4 py-5 mt-12 text-white">
+        <div class="container max-w-6xl py-10 mx-auto">
+
+            <h1 class="text-2xl font-bold leading-tight text-gray-900 sm:text-3xl">
+                Privacy Policy
+            </h1>
+
+            <div class="mt-6 space-y-6 text-lg text-gray-700">
+                <h2 class="text-2xl font-semibold">This is a subtitle</h2>
+                <p>This is the Privacy Policy page. Here you should detail how you handle user data and privacy.</p>
+            </div>
+        </div>
     </div>
 @endsection
