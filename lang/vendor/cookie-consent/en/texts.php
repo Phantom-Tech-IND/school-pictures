5f3f4c3b701c03d4113571d491aa543ac0a51332
infinityatom <19624654+bodnarescu-riccardo@users.noreply.github.com>
2024-04-26 10:17:06 +0300

    Update cookie consent messages to english

--- lang/vendor/cookie-consent/en/texts.php
+++ lang/vendor/cookie-consent/en/texts.php
@@ -1,6 +1,11 @@
 <?php
 
 return [
-    'message' => 'Your experience on this site will be improved by allowing cookies.',
+    'title' => 'Cookies make our life more pleasant',
+    'message' => 'We use cookies and other technologies to operate our websites safely and efficiently, monitor performance, and provide relevant content. With your consent, we use this data for analysis, marketing, and integration of external content, with data also being transferred to third parties. There is a risk that authorities in third countries may access and process this data during data transmission. By clicking "Okay", you agree to the use and transfer of data. Your consent can be revoked at any time. Details in our Cookie and Privacy Policy.',
     'agree' => 'Allow cookies',
+
+    'cookie_policy' => 'Cookie Policy',
+    'privacy_policy' => 'Privacy Policy',
+    'imprint' => 'Imprint',
 ];
