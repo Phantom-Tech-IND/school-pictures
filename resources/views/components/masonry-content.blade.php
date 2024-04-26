61d176fb84f4e7a22867deabe9ec9c5b22daf7e0
Axentioi Alexandru <axentioialexandru95@gmail.com>
2024-04-26 09:36:50 +0300

    Updated homepage

--- resources/views/components/masonry-content.blade.php
+++ resources/views/components/masonry-content.blade.php
@@ -13,18 +13,21 @@
             <div class="flex flex-col-reverse gap-4 md:flex-row">
                 @include('components.icon-card', [
                     'icon' => 'heroicon-o-academic-cap',
-                    'title' => 'Entdecken Sie unsere Angebote!',
                     'description' => 'Starten Sie durch mit unseren Bewerbungsbildern. Professionell, wirkungsvoll, bei Artlinefotografie AG.',
                 ])
-                <div style="background-image: url('{{ asset('gr20-6782-7121.jpg') }}');" class="object-cover w-full bg-center bg-cover h-72">
-                </div>
+               
+               <a class="w-full" href="{{ route('contact') }}">
+                    <div style="background-image: url('{{ asset('banner-3.jpg') }}');" class="object-cover w-full bg-cover h-72">
+                    </div>
+                </a>
             </div>
             <div class="flex flex-col gap-4 md:flex-row">
-                <div style="background-image: url('{{ asset('banner-3.jpg') }}');" class="object-cover w-full bg-cover h-72">
-                </div>
+                <a class="w-full" href="{{ route('contact') }}">
+                    <div style="background-image: url('{{ asset('gr20-6782-7121.jpg') }}');" class="object-cover w-full bg-center bg-cover h-72">
+                    </div>
+                </a>
                 @include('components.icon-card', [
                     'icon' => 'heroicon-o-academic-cap',
-                    'title' => 'Entdecken Sie unsere Angebote!',
                     'description' => 'Mit einem Fotoshooting bei ArtLine Fotografie AG wird jeder Moment unvergesslich.',
                 ])
             </div>
