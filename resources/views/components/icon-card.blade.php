392db1aa05582926758aa4785a3a398d4ffe0e7f
Axentioi Alexandru <axentioialexandru95@gmail.com>
2024-04-26 09:36:50 +0300

    Updated homepage

--- resources/views/components/icon-card.blade.php
+++ resources/views/components/icon-card.blade.php
@@ -2,10 +2,8 @@
     {{-- <div class="w-[70px] h-[70px]">
         <x-heroicon-o-academic-cap class="text-custom-green" style="width: 100%; height: 100%; color: #94c11c" />
     </div> --}}
-    <h2 class="mt-4 text-xl text-center text-white">{{ $title }}</h2>
-    <hr style="margin-bottom: 20px; width: 50%; border-color: #94c11c;">
     {{-- <p class="mt-2 text-sm italic font-light text-white">{{ $heading }}</p> --}}
-    <p class="mt-4 text-sm font-light text-center text-white">
+    <p class="mt-4 text-lg italic font-light text-center text-white">
         {!! $description !!}
     </p>
 </div>
