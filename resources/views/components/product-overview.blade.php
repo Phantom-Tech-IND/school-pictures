bdf6bc6d8c9deac1ee00b1809b689ae189fd4deb
infinityatom <19624654+bodnarescu-riccardo@users.noreply.github.com>
2024-05-04 15:29:50 +0300

    added price

--- resources/views/components/product-overview.blade.php
+++ resources/views/components/product-overview.blade.php
@@ -5,29 +5,27 @@
 @endphp
 
 
-<image-gallery-container
-    {{-- data-min="{{ $selectedProduct['min'] }}" --}}
-    {{-- data-max="{{ $selectedProduct['max'] }}" --}}
-    data-min="{{ 1 }}"
-    data-max="{{ 5 }}"
-    data-selected-product-id="{{ $selectedProductId }}" id="{{ $galleryName }}">
+<image-gallery-container {{-- data-min="{{ $selectedProduct['min'] }}" --}} {{-- data-max="{{ $selectedProduct['max'] }}" --}} data-min="{{ 1 }}"
+    data-max="{{ 5 }}" data-selected-product-id="{{ $selectedProductId }}" id="{{ $galleryName }}">
     <div class="bg-white">
         <div class="pt-6">
             <div
                 class="max-w-2xl grid-rows-1 mx-auto mt-6 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-4 lg:gap-x-8 lg:px-8 lg:h-[80lvh]">
                 <div
                     class="bg-gray-100 border-2 border-gray-300 shadow-lg lg:col-span-2 lg:flex lg:flex-col lg:h-full lg:justify-stretch">
-                    <img src="{{ $selectedProduct['images'][0] }}" id="productImage"
+                    <img src="{{ $selectedProduct['images'][0] ?? '' }}" id="productImage"
                         alt="Two each of gray, white, and black shirts laying flat."
                         class="flex-shrink object-contain object-center w-full max-w-full min-h-0 p-4 rounded-lg lg:grow lg:h-auto h-72">
                     <image-display container-id="{{ $galleryName }}"
                         class="flex flex-row flex-shrink-0 overflow-x-auto"></image-display>
                 </div>
 
-                <div class="relative grid grid-cols-3 gap-4 p-4 mt-8 overflow-y-scroll border-2 border-gray-300 shadow-lg lg:mt-0 max-h-72 lg:max-h-max lg:h-auto auto-rows-min place-items-center lg:col-span-2 lg:grid-cols-4 xs:grid-cols-4">
+                <div
+                    class="relative grid grid-cols-3 gap-4 p-4 mt-8 overflow-y-scroll border-2 border-gray-300 shadow-lg lg:mt-0 max-h-72 lg:max-h-max lg:h-auto auto-rows-min place-items-center lg:col-span-2 lg:grid-cols-4 xs:grid-cols-4">
                     @foreach ($student['photos'] as $key => $photo)
                         <image-selector container-id="{{ $galleryName }}" alt="Image {{ $key + 1 }}"
-                            key="{{ $photo['id'] }}" src="{{ $photo['photo_path'] }}" class="w-24 h-24"></image-selector>
+                            key="{{ $photo['id'] }}" src="{{ $photo['photo_path'] }}"
+                            class="w-24 h-24"></image-selector>
                     @endforeach
                 </div>
 
@@ -37,32 +35,36 @@
                 class="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-4 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
 
                 <div class="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-                    <h1 id="productName" x-text="selectedProductName" class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{{ $selectedProduct['name'] }}</h1>
+                    <h1 id="productName" x-text="selectedProductName"
+                        class="inline text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+                        {{ $selectedProduct['name'] }}
+                    </h1>
+                    <span id="productPrice" class="text-lg text-gray-500">{{ $selectedProduct['price'] }} CHF</span>
                 </div>
 
                 <div class="mt-4 lg:row-span-3 lg:mt-0 lg:col-span-2">
                     <h2 class="sr-only">Product information</h2>
-                    <p class="text-3xl tracking-tight text-gray-900">Some title here</p>
+
 
                     <div class="mt-10">
                         <div x-data="{
                             selectedProductId: '{{ $selectedProductId }}',
                             selectedProductName: '{{ $selectedProduct['name'] }}',
-                            selectedProductDescription: '{{ $selectedProduct['description'] }}',
+                            selectedProductDescription: '{{ str_replace(PHP_EOL, ' ', $selectedProduct['description']) }}',
                             updateProducts() {
-                                console.log('Selected Product ID:', this.selectedProductId);
-
                                 var productImage = document.getElementById('productImage');
                                 var selectedProductId = document.getElementById('y-selected-product-' + this.selectedProductId);
                                 var productDescription = document.getElementById('productDescription');
                                 var productName = document.getElementById('productName');
-
+                                var productPrice = document.getElementById('productPrice');
+                        
                                 if (productImage && selectedProductId) {
                                     productImage.src = selectedProductId.src;
                                     this.selectedProductName = selectedProductId.getAttribute('data-name');
                                     productName.textContent = this.selectedProductName;
-
+                        
                                     productDescription.textContent = selectedProductId.getAttribute('data-description');
+                                    productPrice.textContent = selectedProductId.getAttribute('data-price') + ' CHF';
                                 }
                                 var gallery = document.getElementById('{{ $galleryName }}');
                                 gallery.setAttribute('data-min', selectedProductId.getAttribute('data-min'));
@@ -93,12 +95,12 @@
                                                 aria-labelledby="product-choice-{{ $product['id'] }}-label">
                                             <span id="product-choice-{{ $product['id'] }}-label"
                                                 class="sr-only">{{ $product['id'] }}</span>
-                                            <img src="{{ $product['images'][0] }}" alt=""
+                                            <img src="{{ $product['images'][0] ?? '' }}" alt=""
                                                 id="y-selected-product-{{ $product['id'] }}"
-                                                data-min="{{ 1 }}"
-                                                data-max="{{ 5 }}"
+                                                data-min="{{ 1 }}" data-max="{{ 5 }}"
                                                 data-name="{{ $product['name'] }}"
                                                 data-description="{{ $product['description'] }}"
+                                                data-price="{{ $product['price'] }}"
                                                 class="object-cover w-full h-full transition-opacity duration-300 ease-in-out rounded-md">
                                             <div
                                                 class="absolute inset-0 transition-all duration-300 ease-in-out bg-black bg-opacity-0 group-hover:bg-opacity-50">
@@ -121,27 +123,8 @@
                         <h3 class="sr-only">Description</h3>
 
                         <div class="space-y-6">
-                            <p id="productDescription" x-text="selectedProductDescription" class="text-base text-gray-900">{{ $selectedProduct['description'] }}</p>
-                        </div>
-                    </div>
-
-                    <div class="mt-10">
-                        <h3 class="text-sm font-medium text-gray-900">Package Contents</h3>
-
-                        <div class="mt-4">
-                            <ul role="list" class="pl-4 space-y-2 text-sm list-disc">
-                                <li class="text-gray-600">2 x Black Basic Tees</li>
-                                <li class="text-gray-600">2 x White Basic Tees</li>
-                                <li class="text-gray-600">2 x Heather Gray Basic Tees</li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="mt-10">
-                        <h2 class="text-sm font-medium text-gray-900">Details</h2>
-
-                        <div class="mt-4 space-y-6">
-                            <p class="text-sm text-gray-600">Some details about the product</p>
+                            <p id="productDescription" x-text="selectedProductDescription"
+                                class="text-base text-gray-900">{{ $selectedProduct['description'] }}</p>
                         </div>
                     </div>
                 </div>
