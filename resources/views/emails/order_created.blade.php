8129400962e1c887777cf49d7483263dac23c929
Axentioi Alexandru <101560586+alexanderaxentioi@users.noreply.github.com>
2024-05-30 11:46:58 +0300

    added text

--- resources/views/emails/order_created.blade.php
+++ resources/views/emails/order_created.blade.php
@@ -1,48 +1,48 @@
 @component('mail::message')
-@if ($emailRole == 'user')
-# Order Confirmation
+    @if ($emailRole == 'user')
+        # Order Confirmation
 
-Hello **{{ $contact->name }}**,<br>
-<br>
-Thank you for your order! Here are the details:<br>
-Your order number is **{{ $order->id }}**<br>
-Total {{ $order->amount }} CHF<br>
-@if ($order->payment_method === 'bank_transfer')
-Please transfer the amount to the following account:<br>
-IBAN: {{ env('BANK_IBAN') }}<br>
-@endif
-@endif
-@if ($emailRole == 'admin')
-# New order from {{ $contact->name }}<br>
-@endif
+        Hello **{{ $contact->name }}**,<br>
+        <br>
+        Thank you for your order! Here are the details:<br>
+        Your order number is **{{ $order->id }}**<br>
+        Total {{ $order->amount }} CHF<br>
+        @if ($order->payment_method === 'bank_transfer')
+            Please transfer the amount to the following account:<br>
+            IBAN: {{ env('BANK_IBAN') }}<br>
+            Please use the reference number of the order as the reference for the transfer: {{ $order->id }}<br>
+        @endif
+    @endif
+    @if ($emailRole == 'admin')
+        # New order from {{ $contact->name }}<br>
+    @endif
 
-@if ($emailRole == 'admin')
-@component('mail::button', ['url' => url('admin/orders')])
-View Orders
+    @if ($emailRole == 'admin')
+        @component('mail::button', ['url' => url('admin/orders')])
+            View Orders
+        @endcomponent
+    @endif
+    <br>
+
+    ## Items Ordered:
+    <hr>
+
+    @foreach ($items as $item)
+        @component('mail::product', [
+            'message' => $message,
+            'imageUrl' => isset($item->product->images[0]) ? storage_path('app/public/') . $item->product->images[0] : '',
+            'name' => $item->product->name,
+            'price' => $item->price,
+            'quantity' => $item->quantity,
+            'checkboxes' => $item->options['checkbox'] ?? [],
+            'selects' => $item->options['selects'] ?? [],
+            'files' => $item->options['files'] ?? [],
+        ])
+        @endcomponent
+        <hr>
+    @endforeach
+
+    @if ($emailRole == 'user')
+        # Thanks, {{ config('app.name') }}
+    @endif
 @endcomponent
-@endif
-<br>
-
-## Items Ordered:
-<hr>
-
-@foreach ($items as $item)
-@component('mail::product', [
-	'message' => $message,
-	'imageUrl' => isset($item->product->images[0]) ? storage_path('app/public/') . $item->product->images[0] : '',
-	'name' => $item->product->name,
-	'price' => $item->price,
-	'quantity' => $item->quantity,
-	'checkboxes' => $item->options['checkbox'] ?? [],
-	'selects' => $item->options['selects'] ?? [],
-	'files' => $item->options['files'] ?? []
-])
-@endcomponent
-<hr>
-@endforeach
-
-@if ($emailRole == 'user')
-# Thanks, {{ config('app.name') }}
-@endif
-@endcomponent
-
